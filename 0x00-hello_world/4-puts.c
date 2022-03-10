d6909c8c705e1bd042622fec702e67fece9942fd
Kimaiyo85 <dorokimm@gmail.com>
2022-03-10 12:56:36 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
+nclude <stdio.h>
+
+
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}#include <stdio.h>
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}#include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
