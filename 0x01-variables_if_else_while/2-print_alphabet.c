c78672252f76d5b24316ed9c5cae05445102f1ed
Kimaiyo85 <dorokimm@gmail.com>
2022-03-14 11:25:32 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio,h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+		putchar(c);
+		put char('\n');
+
+
+return (0);
+
+}
